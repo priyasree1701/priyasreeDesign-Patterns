c67179f4edfa5fd74a6d1b2f5043688743dccf83
priyasree1701 <priyasree.manthri@gmail.com>
2020-07-21 16:51:39 +0530

    task-7

---
+++ Singleton_pattern/src/main/java/com/epam/Singleton_pattern/Demo_Singleton.java
@@ -0,0 +1,19 @@
+package com.epam.Singleton_pattern;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class Demo_Singleton{
+	   public static void main(String[] args) {
+
+	      //illegal construct
+	      //Compile Time Error: The constructor SingleObject() is not visible
+	      //SingleObject object = new SingleObject();
+
+	      //Get the only object available
+	      Singleton obj = Singleton.getInstance();
+	      obj.show();
+	   }
+	}
---
+++ Singleton_pattern/src/main/java/com/epam/Singleton_pattern/Singleton.java
@@ -0,0 +1,13 @@
+package com.epam.Singleton_pattern;
+
+public class Singleton {
+	   private static Singleton s = new Singleton();
+	   private Singleton(){}
+	   public static Singleton getInstance(){
+	      return s;
+	   }
+
+	   public void show(){
+	      System.out.println("demo of singleton pattern type");
+	   }
+	}
